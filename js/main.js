250a68b83440046964da22ffa6b4bfd7e939bdf8
Muhammad <m.ihsanaziz1@gmail.com>
2022-05-08 03:46:21 -0600

    Fill grid with nested loop

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
 //PERIODIC TABLE
 
-//Create Array of Elements
+//CREATE ARRAY OF ELEMENTS
 let elements;
 fetch("../data/periodic-table-data.json")
   .then((rawData) => rawData.json())
   .then((data) => (elements = data));
 
+//DOCUMENT ELEMENTS
+const tableGrid = document.getElementById("periodic-table");
+
+document.addEventListener("DOMContentLoaded", fillGrid);
+
+//GRID
+//Multidimensional Array
+var x = new Array(10);
+
+function fillGrid() {
+  //10 * 18 grid
+  for (let i = 0; i < 180; i++) {
+    const elementBlock = document.createElement("div");
+    elementBlock.classList.add("grid-item");
+    tableGrid.appendChild(elementBlock);
+  }
+}
