6eff7b18cc8caad4a16f3619e5322dd3620b7e05
MUHAMMAD AZIZ <m.aziz5@share.epsb.ca>
2022-05-09 14:47:42 -0600

    Create append loop

--- js/main.js
+++ js/main.js
@@ -8,18 +8,56 @@ fetch("../data/periodic-table-data.json")
 
 //DOCUMENT ELEMENTS
 const tableGrid = document.getElementById("periodic-table");
-
 document.addEventListener("DOMContentLoaded", fillGrid);
 
 //GRID
-//Multidimensional Array
-var x = new Array(10);
-
 function fillGrid() {
   //10 * 18 grid
-  for (let i = 0; i < 180; i++) {
+  let countLow = 0;
+  let rowLength = 18;
+  let countHigh = rowLength;
+
+  for (let i = countLow; i < countHigh; i++) {
     const elementBlock = document.createElement("div");
     elementBlock.classList.add("grid-item");
     tableGrid.appendChild(elementBlock);
+    countLow++;
   }
+  countHigh += rowLength;
+
+  for (let i = countLow; i < countHigh; i++) {
+    const elementBlock = document.createElement("div");
+    elementBlock.classList.add("grid-item");
+    tableGrid.appendChild(elementBlock);
+    countLow++;
+  }
+  countHigh += rowLength;
+
+  for (let i = countLow; i < countHigh; i++) {
+    const elementBlock = document.createElement("div");
+    elementBlock.classList.add("grid-item");
+    tableGrid.appendChild(elementBlock);
+    countLow++;
+  }
+  console.log(countLow, countHigh);
 }
+
+function appendLoop() {
+  let countLow = 0;
+  let rowLength = 18;
+  let countHigh = rowLength;
+
+  for (let i = countLow; i < countHigh; i++) {
+    const elementBlock = document.createElement("div");
+    elementBlock.classList.add("grid-item");
+    tableGrid.appendChild(elementBlock);
+    countLow++;
+  }
+  
+}
+
+// for (let i = 0; i < 180; i++) {
+//   const elementBlock = document.createElement("div");
+//   elementBlock.classList.add("grid-item");
+//   tableGrid.appendChild(elementBlock);
+// }
