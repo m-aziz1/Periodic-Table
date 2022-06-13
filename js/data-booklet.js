2161de08576cc3b1b6355c9db94f68dd08239293
Muhammad <m.ihsanaziz1@gmail.com>
2022-06-12 22:55:18 -0600

    Create data booklet page and search function

---
+++ js/data-booklet.js
@@ -0,0 +1,55 @@
+//DATA BOOKLET PAGES
+let pages = [
+  "chemistry notation",
+  "miscellaneous",
+  "standard molar enthalpies of formation",
+  "standard molar enthalpies of formation continued",
+  "solubility of some common ionic compounds in water flame colour of elements",
+  "table of selected standard electrode potentials",
+  "relative strengths of acids and bases",
+  "relative strengths of acids and bases continued",
+  "acid-base indicators",
+  "colours of common aqueous Ions",
+];
+
+//SEARCHABLE LIST
+let objects = [];
+
+//APPEND IMAGES
+const pagesContainer = document.getElementById("pages-container");
+for (let i = 0; i < pages.length; i++) {
+  let number = i + 2;
+
+  const h2 = Object.assign(document.createElement("h2"), {
+    innerHTML: `page ${number}`,
+  });
+
+  const img = Object.assign(document.createElement("img"), {
+    classList: "page-image",
+    src: `../images/page-${number}.png`,
+    alt: `Page ${number}`,
+    id: `${pages[i]}`,
+  });
+
+  objects.push({
+    page: h2,
+    element: img,
+  });
+
+  pagesContainer.append(h2, img);
+}
+
+//SEARCH BAR
+//call whenever value changes
+const searchInput = document.getElementById("page-search");
+searchInput.addEventListener("input", (event) => {
+  const value = event.target.value.toLowerCase();
+  for (let i = 0; i < objects.length; i++) {
+    const show =
+      objects[i]["page"]["innerHTML"].includes(value) ||
+      objects[i]["element"]["id"].includes(value);
+
+    objects[i]["page"].classList.toggle("hide", !show);
+    objects[i]["element"].classList.toggle("hide", !show);
+  }
+});
